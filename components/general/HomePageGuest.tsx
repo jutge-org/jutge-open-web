f7e20c4a7f507c392f99c03071df55ee125fad6f
Jordi Petit <jordi.petit@gmail.com>
2026-07-04 17:24:44 +0200

    User modern jutge icon in guest home page

--- components/general/HomePageGuest.tsx
+++ components/general/HomePageGuest.tsx
@@ -8,6 +8,7 @@ import MainBreadcrumbs from '@/components/general/MainBreadcrumbs'
 import { countActiveProglangs, getActiveCompilers } from '@/lib/documentation'
 import { fetchHomepageStats } from '@/services/queries/misc'
 import { fetchCompilers } from '@/services/queries/tables'
+import Image from 'next/image'
 
 export async function HomePageGuest() {
     const [homepageStats, compilers] = await Promise.all([fetchHomepageStats(), fetchCompilers()])
@@ -25,7 +26,8 @@ export async function HomePageGuest() {
             <HomeYearsRibbon />
             <MainBreadcrumbs breadcrumbs={[{ title: 'Jutge.org', url: '/' }]} />
             <div className="flex flex-col items-center space-y-4 pt-8 text-center">
-                <h1 className="inline-block bg-linear-to-r from-cyan-300 via-sky-400 to-blue-500 bg-clip-text pb-1 text-5xl leading-[1.2] font-thin tracking-wide text-balance text-transparent sm:text-6xl">
+                <Image src="/jutge/modern.png" alt="Jutge.org" width={250} height={250} className="-mb-2" />
+                <h1 className="inline-block bg-linear-to-r from-cyan-300 via-sky-400 to-blue-500 bg-clip-text pb-1 leading-[1.2] font-thin tracking-wide text-balance text-transparent sm:text-8xl">
                     Jutge.org
                 </h1>
                 <p className="max-w-2xl text-xl text-pretty font-thin text-muted-foreground italic sm:text-2xl">

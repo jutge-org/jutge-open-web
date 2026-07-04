421a217c4038d4c2d1abd0047b23d29f0fcc92d6
Jordi Petit <jordi.petit@gmail.com>
2026-07-04 21:26:24 +0200

    Add effect

--- components/general/HomePageGuest.tsx
+++ components/general/HomePageGuest.tsx
@@ -26,7 +26,13 @@ export async function HomePageGuest() {
             <HomeYearsRibbon />
             <MainBreadcrumbs breadcrumbs={[{ title: 'Jutge.org', url: '/' }]} />
             <div className="flex flex-col items-center space-y-4 pt-8 text-center">
-                <Image src="/jutge/modern.png" alt="Jutge.org" width={250} height={250} className="-mb-2" />
+                <Image
+                    src="/jutge/modern.png"
+                    alt="Jutge.org"
+                    width={250}
+                    height={250}
+                    className="-mb-2 hover:ease-in-out hover:duration-300 hover:scale-125"
+                />
                 <h1 className="inline-block bg-linear-to-r from-cyan-300 via-sky-400 to-blue-500 bg-clip-text pb-1 leading-[1.2] font-thin tracking-wide text-balance text-transparent sm:text-8xl">
                     Jutge.org
                 </h1>

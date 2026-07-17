5c8827e77af9691e92fb7e50146fc806c10495ae
Pau Fernández <pau.fernandez@upc.edu>
2026-07-17 19:50:29 +0200

    Fixed JUTGE_API_URL variable
    - Also fixed the .env.sample (was duplicated).

--- lib/jutge.ts
+++ lib/jutge.ts
@@ -3,7 +3,7 @@
 import { JutgeApiClient } from './jutge_api_client'
 
 const jutge = new JutgeApiClient()
-
+jutge.JUTGE_API_URL = process.env.NEXT_PUBLIC_JUTGE_API_URL || 'https://api.jutge.org/api'
 jutge.clientTTLs.set('problems.getAllAbstractProblems', 300)
 jutge.clientTTLs.set('problems.getAllProblems', 300)
 

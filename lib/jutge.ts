f9a6eb53a0c87a0a1d49abcf17921bd906dc9ce1
Jordi Petit <jordi.petit@gmail.com>
2026-07-29 19:05:26 +0200

    Add time

--- lib/jutge.ts
+++ lib/jutge.ts
@@ -4,7 +4,7 @@ import { JutgeApiClient } from './jutge_api_client'
 
 const jutge = new JutgeApiClient()
 jutge.JUTGE_API_URL = process.env.NEXT_PUBLIC_JUTGE_API_URL || 'https://api.jutge.org/api'
-jutge.clientTTLs.set('problems.getAllAbstractProblems', 300)
-jutge.clientTTLs.set('problems.getAllProblems', 300)
+jutge.clientTTLs.set('problems.getAllAbstractProblems', 3600)
+jutge.clientTTLs.set('problems.getAllProblems', 3600)
 
 export default jutge
